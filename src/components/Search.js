24721f4adf218e5f128258d5446f13e5ee27ef8b
Jc7 <jhuinac7@gmail.com>
2020-11-02 08:15:38 -0800

    saving..

--- src/components/Search.js
+++ src/components/Search.js
@@ -57,7 +57,7 @@ class PostList extends React.Component {
         }
     }
     getAllPosts(cityName) {
-        axios.get("https://fp-garage-api.herokuapp.com/byName/" + cityName).then((response) => {
+        axios.get("https://fp-garage-api.herokuapp.com/posts/byName/" + cityName).then((response) => {
             this.setState({
                 posts: response.data,
             })
